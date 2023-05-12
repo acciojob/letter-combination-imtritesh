3ea106598e38398a410f91dd829b1922b004e552
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-12 12:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+	if (digits.length === 0) return [];
+
+  const mapping = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  const result = [];
+
+  const backtrack = (combination, nextDigits) => {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const digit = nextDigits[0];
+      const letters = mapping[digit];
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, nextDigits.slice(1));
+      }
+    }
+  };
+
+  backtrack('', digits);
+  return result.sort();
 }
 
 module.exports = letterCombinations;
